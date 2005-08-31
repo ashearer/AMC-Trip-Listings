af624a0ed3f3fa343ad6b3b48c218032e07630fb
Andrew Shearer <andrew@ashearer.com>
2005-08-31 00:40:50 +0000

    2005-08-31delta

--- buildfromxml.sh
+++ buildfromxml.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd "$(dirname "$0")"
-~/Library/Frameworks/xsltproc --novalid --output ./trip_list.shtml ./trips-xml-to-html-page.xsl ./trip_listings.xml
-~/Library/Frameworks/xsltproc --novalid --output ./trip_list_email.html ./trips-xml-to-html-email.xsl ./trip_listings.xml
+xsltproc --novalid --output ./trip_list.shtml ./trips-xml-to-html-page.xsl ./trip_listings.xml
+xsltproc --novalid --output ./trip_list_email.html ./trips-xml-to-html-email.xsl ./trip_listings.xml
