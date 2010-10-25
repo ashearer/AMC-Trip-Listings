1fd9f7040fef8513ed2cd4b01054f1e6c94cabe8
Andrew Shearer <andrew@ashearer.com>
2010-10-25 00:13:44 +0000

    2010-10-24delta update config

--- _private/config.inc.php
+++ _private/config.inc.php
@@ -1,27 +1,27 @@
 <?php
 /*if ($groupID == 'boston') {
-    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010";
+    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015";
     $groupTitle = 'AMC Boston';
     $groupHomePageURL = 'http://amcboston.org/';
 }
 elseif ($groupID == 'bostonym') {
-    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010";
+    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2012";
     $groupTitle = 'AMC Boston Young Members';
     $groupHomePageURL = 'http://amcboston.org/youngmembers/';
 }
 elseif ($groupID == 'worcester') {
-    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010";
+    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015";
     //$xmlPath = "http://trips.outdoors.org/xml/Narrtrips.xml";
     $groupTitle = 'AMC Worcester';
     $groupHomePageURL = 'http://amcworcester.org/';
 }
 elseif ($groupID == 'worcesterym') {
-    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010";
+    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015";
     $groupTitle = 'AMC Worcester Young Members';
     $groupHomePageURL = 'http://amcworcester.org/ym/';
 }
 else {
-    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Narragansett&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010";
+    $xmlPath = "http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Narragansett&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015";
     $groupID = 'narragansett';
     $groupTitle = 'AMC Narragansett';
     $groupHomePageURL = 'http://amcnarragansett.org/';
@@ -35,7 +35,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://ashearer.com/amc/listings/?c=boston',
         'rssUrl' => 'http://ashearer.com/amc/listings/?c=boston&output=rss',
         'icsUrl' => 'http://ashearer.com/amc/listings/?c=boston&output=ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
     'bostonym' => array(
         'title' => 'AMC Boston Young Members',
@@ -44,7 +44,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://amcboston.org/youngmembers/trip_list.shtml',
         'rssUrl' => 'http://amcboston.org/youngmembers/trips.rss',
         'icsUrl' => 'http://amcboston.org/youngmembers/trips.ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
         /*
     'bostonymtemp' => array(
@@ -60,7 +60,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://ashearer.com/amc/listings/?c=worcester',
         'rssUrl' => 'http://ashearer.com/amc/listings/?c=worcester&output=rss',
         'icsUrl' => 'http://ashearer.com/amc/listings/?c=worcester&output=ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
     'worcesterym' => array(
         'title' => 'AMC Worcester Young Members',
@@ -69,7 +69,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://ashearer.com/amc/listings/?c=worcesterym',
         'rssUrl' => 'http://ashearer.com/amc/listings/?c=worcesterym&output=rss',
         'icsUrl' => 'http://ashearer.com/amc/listings/?c=worcesterym&output=ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Worcester&tripregion=&tripstate=&tripdestination=&tripcommittee=Young%20Members&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
     'narragansett' => array(
         'title' => 'AMC Narragansett',
@@ -78,7 +78,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://ashearer.com/amc/listings/?c=narragansett',
         'rssUrl' => 'http://ashearer.com/amc/listings/?c=narragansett&output=rss',
         'icsUrl' => 'http://ashearer.com/amc/listings/?c=narragansett&output=ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Narragansett&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Narragansett&tripregion=&tripstate=&tripdestination=&tripcommittee=&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
     'hbboston' => array(
         'title' => 'AMC Boston H/B',
@@ -87,7 +87,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://ashearer.com/amc/listings/?c=hbboston',
         'rssUrl' => 'http://ashearer.com/amc/listings/?c=hbboston&output=rss',
         'icsUrl' => 'http://ashearer.com/amc/listings/?c=hbboston&output=ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Hiking/Backpacking&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Hiking/Backpacking&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
     'bostonintro' => array(
         'title' => 'AMC Boston Intro',
@@ -96,7 +96,7 @@ $kGroupData = array(
         'listingsUrl' => 'http://ashearer.com/amc/listings/?c=bostonintro',
         'rssUrl' => 'http://ashearer.com/amc/listings/?c=bostonintro&output=rss',
         'icsUrl' => 'http://ashearer.com/amc/listings/?c=bostonintro&output=ics',
-        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Intro&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2005&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2010',
+        'xmlUrl' => 'http://trips.outdoors.org/index.cfm?method=ch.doxml&searchterms=&searchwhich=AND&whichsearch=byterms&tripchapter=Boston&tripregion=&tripstate=&tripdestination=&tripcommittee=Intro&tripactivity=&tripstartmonth=7&tripstartday=1&tripstartyear=2010&tripstartmonthb=0&tripstartdayb=0&tripstartyearb=2015',
         ),
 
 );
